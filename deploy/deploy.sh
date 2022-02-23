999efef57f9f7077cbae189ebb6e0704c6aa3a54
Kai <kejinko012@gmail.com>
2022-02-23 15:43:08 +0100

    added birmingham in index

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DEPLOY_SERVER=$DEPLOY_SERVER
+DEPLOY_SERVER=$SERVER_IPADDRESS
 
 echo "Deploying to ${SERVER_IPADDRESS}"
 ssh root@${SERVER_IPADDRESS} 'bash' < ./deploy/server.sh
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'GeoQuiz' });
+  res.render('index', { title: 'GeoQuiz', location: "Birmingham" });
 });
 
 module.exports = router;
