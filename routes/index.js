1d7e95c0dca5968656873897b24c0acbeb93daa1
Kai <kejinko012@gmail.com>
2022-02-24 04:57:38 +0100

    enable navigator object in nodejs

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,25 @@
 var express = require('express');
+const { Navigator } = require("node-navigator");
+const navigator = new Navigator();
 var router = express.Router();
 
-/* GET home page. */
+
+
+
+
+function error(err) {
+  console.warn(`ERROR(${err.code}): ${err.message}`);
+}
+
+navigator.geolocation.getCurrentPosition((success, error) => {
+  if (error) console.error(error);
+  else console.log(success);
+
+  
+});
+
+
+
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'GeoQuiz', location: "Birmingham" });
 });
