1b21dd26763a66e4e25151e8a3c83b8633a77f7b
Kai <kejinko012@gmail.com>
2022-02-25 13:04:37 +0100

    added api and geocoding

--- routes/index.js
+++ routes/index.js
@@ -1,27 +1,43 @@
 var express = require('express');
 const { Navigator } = require("node-navigator");
+const NodeGeocoder = require('node-geocoder');
 const navigator = new Navigator();
 var router = express.Router();
 
+const API_KEY = "AIzaSyDDsgTeob3znloELiDV-r1rFQl3vsq2ap0"
+const options = {
+  provider: 'google',
+
+  // Optional depending on the providers
+
+  apiKey: API_KEY, // for Mapquest, OpenCage, Google Premier
+  formatter: null // 'gpx', 'string', ...
+};
+
+const geocoder = NodeGeocoder(options);
 
 
 
-
-function error(err) {
-  console.warn(`ERROR(${err.code}): ${err.message}`);
-}
-
-navigator.geolocation.getCurrentPosition((success, error) => {
-  if (error) console.error(error);
-  else console.log(success);
-
-  
-});
 
 
 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'GeoQuiz', location: "Birmingham" });
+  
+ 
+  navigator.geolocation.getCurrentPosition((success, error) => {
+    if (error) console.error(error);
+    else console.log(success);
+    async function getResponse() {
+      const response = await geocoder.reverse({ lat: success.latitude, lon: success.longitude });
+      res.render('index', { title: 'GeoQuiz', location:response[0].city });
+    }
+    
+    getResponse()
+  });
 });
+
+
+
+
 
 module.exports = router;
