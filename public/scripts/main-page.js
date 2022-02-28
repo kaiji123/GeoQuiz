9f28f42c114cf4c48517bcc630ce5999d6642165
Jack Jazrawy-Brown <sunspiremusic@gmail.com>
2022-02-28 13:22:47 +0000

    api location route finished

--- public/scripts/main-page.js
+++ public/scripts/main-page.js
@@ -1,15 +1,44 @@
-//run when index is loaded - should inti
-function init(){
-    //check if a user is logged in + set header to username
-    if(sessionStorage.getItem("user") != null){
-        $('#username').html(sessionStorage.getItem("user"));
-    }
-    else{
-        $('#username').html('Guest');
+//check if a user is logged in and display their name
+function showUser(){
+        //check if a user is logged in + set header to username
+        if(sessionStorage.getItem("user") != null){
+            $('#username').append(sessionStorage.getItem("user").split(' ')[0]);
+        }
+        else{
+            $('#username').append('Guest');
+        }
+}
+
+async function showClientLocation(){
+    //check gelocation is available
+    if('geolocation' in navigator) {
+        var data = navigator.geolocation.getCurrentPosition((position) => {
+            //get lat and lon coords
+            var coords = [position.coords.latitude, position.coords.longitude];
+            var data = {
+                    lat: position.coords.latitude,
+                    lon: position.coords.longitude
+                };       
+            //create new POST request to location api using fetch (at the moment it just bounces back data)
+            fetch('/api/location',{
+                method: 'POST',
+                headers: {'Accept': 'application/json', 'Content-Type': 'application/json'},
+                body: JSON.stringify(data),
+            })
+            .then(res => res.json())
+            .then(data => {
+                    $('#location').append(data.city)
+                }
+            );
+        });
+    } else {
+        console.log('Location services not availabe');
+        return null;
     }
 }
 
 //run on document load
 $(function(){
-    init();
+    showUser();
+    showClientLocation();
 })
--- routes/api.js
+++ routes/api.js
@@ -7,17 +7,25 @@ const options = {
   provider: 'google',
   // Optional depending on the providers
 
-  apiKey: process.env.API_KEY, // for Mapquest, OpenCage, Google Premier
+  apiKey: 'AIzaSyBsco_UzL1CA7GKB5mXD4_IYuOjTTLY7tQ', // for Mapquest, OpenCage, Google Premier
   formatter: null // 'gpx', 'string', ...
 };
 
 const geocoder = NodeGeocoder(options);
 
 //returns an address from given coordinates
-router.post('/location', function(req, res, next) {
-    //TODO: get address using maps api
-    res.send(req.body);
+router.post('/location', async (req, res) => {
+    var coords = req.body;
+    var location = await locFromCoords(coords);
+    console.log(location);
+    res.send(location[0]);
+
 });
+
+async function locFromCoords(coords){
+  const loc = await geocoder.reverse(coords);
+  return loc;
+}
 
 router.post('/login', function(req, res, next) {
     user = req.body.username
