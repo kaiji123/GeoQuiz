c9be66393263d45a4a08503e5e63a962449b84aa
Jack Jazrawy-Brown <sunspiremusic@gmail.com>
2022-02-28 11:18:47 +0000

    Fixed session storage not being set

--- public/scripts/client-login.js
+++ public/scripts/client-login.js
@@ -3,6 +3,6 @@ function onSignIn(googleUser) {
     var profile = googleUser.getBasicProfile();
     
     //check if the user exists in our database
-    sessionStorage.setitem("id", profile.getId());
+    sessionStorage.setItem("id", profile.getId());
     sessionStorage.setItem("user", profile.getName());
 }
