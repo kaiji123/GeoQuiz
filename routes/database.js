49bc314b65c6d2c884cff064056a1a48c55b1aa8
Kai <kejinko012@gmail.com>
2022-03-02 09:01:54 +0100

    hello

--- routes/database.js
+++ routes/database.js
@@ -2,23 +2,30 @@
  
 module.exports = {
   selectUID: function(){
-    var mysql      = require('mysql');
+    var mysql      = require('mysql2');
+    var fs = require('fs')
     var connection = mysql.createConnection({
-      host     : 'db-mysql-lon1-92192-do-user-10957123-0.b.db.ondigitalocean.com',
+      host     : 'db-mysql-lon1-72184-do-user-10942530-0.b.db.ondigitalocean.com',
       port     : '25060',
       user     : 'doadmin',
-      password : 'MBAeLg2tttYUGn2Z',
-      database : 'defaultdb'
+      password : 'R45mUKjM0QGNmejm',
+      database: 'defaultdb',
+      ssl      : {
+        ca : fs.readFileSync('./ca-certificate.crt')
+      }
     });
 
     connection.connect(function(err) {
       if (err) throw err;
-      connection.query("SELECT * FROM users", function (err, result, fields) {
+      connection.query("SELECT * FROM test_table", function (err, result, fields) {
         if (err) throw err;
-        return result;
+        console.log(result);
+        
+ 
+        connection.end();
       });
     });
     
-    connection.end();
+  
   }
 }
