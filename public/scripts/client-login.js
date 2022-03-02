880a7efd97569d49394c4c62f9403d095d55cead
Maria <mxm1303@student.bham.ac.uk>
2022-03-02 12:34:38 +0000

    improved text scaling, playing around w/ fb OAuth

--- public/scripts/client-login.js
+++ public/scripts/client-login.js
@@ -8,3 +8,28 @@ function onSignIn(googleUser) {
 
     //window.location.href = '/index'
 }
+
+window.fbAsyncInit = function() {
+    // FB JavaScript SDK configuration and setup
+    FB.init({
+      appId      : '589355126092251', // FB App ID
+      //cookie     : true,  // enable cookies to allow the server to access the session
+      xfbml      : true,  // parse social plugins on this page
+      version    : 'v3.2' // use graph api version 2.8
+    });
+
+  
+};
+
+// Load the JavaScript SDK asynchronously
+(function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+
+
+
