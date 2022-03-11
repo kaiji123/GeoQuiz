97855eaf0169c625df6d1cde034ce0f8635acaca
Kai <kejinko012@gmail.com>
2022-03-11 09:35:06 +0100

    env var

--- routes/database.js
+++ routes/database.js
@@ -2,11 +2,11 @@ const res = require('express/lib/response');
 var mysql      = require('mysql2');
 var fs = require('fs')
 var connection = mysql.createConnection({
-  host     : 'db-mysql-lon1-72184-do-user-10942530-0.b.db.ondigitalocean.com',
-  port     : '25060',
-  user     : 'doadmin',
-  password : 'R45mUKjM0QGNmejm',
-  database: 'defaultdb',
+  host     : process.env.HOSTNAME,
+  port     : process.env.DBPORT,
+  user     : process.env.DBUSER,
+  password : process.env.DBPASSWORD,
+  database: process.env.DBDATABASE,
   ssl      : {
     ca : fs.readFileSync('./ca-certificate.crt')
   }
@@ -75,7 +75,7 @@ module.exports = {
   
 
 
-      connection.query("SELECT users.name,scores.scores from scores inner join users on scores.id = users.id order by scores.scores desc", function (err, result, fields) {
+      connection.query("SELECT users.name,SUM(scores.scores) from scores inner join users on scores.id = users.id group by scores.id order by scores.scores desc", function (err, result, fields) {
         if (err) throw err;
         console.log(result);
         res.send({"top5":result})
