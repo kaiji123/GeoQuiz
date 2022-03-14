3c18b61bd0350fe8dc301543501c433a6247802a
Kai <kejinko012@gmail.com>
2022-03-14 11:33:50 +0100

    package json lock removed

--- public/scripts/client-login.js
+++ public/scripts/client-login.js
@@ -19,7 +19,7 @@ function onSignIn(googleUser) {
 function signOut() {
 
    
-        var auth2 = gapi.auth2.getAuthInstance();
+    var auth2 = gapi.auth2.getAuthInstance();
         auth2.signOut().then(function () {
         console.log('User signed out.');
     })
