ff6ee0699e4c27a4ff1562f19b90d22197362c37
Kai <kejinko012@gmail.com>
2022-03-17 15:28:40 +0100

    catch block

--- acceptance.js
+++ acceptance.js
@@ -5,13 +5,14 @@ const should = chai.should();
 const expect = chai.expect;
 require("chromedriver")
 var webdriver = require('selenium-webdriver');
-var driver = new webdriver.Builder()
-  .forBrowser('chrome')
-  .build();
+
 
 
 async function testBrowser(){
 try {
+  var driver = new webdriver.Builder()
+  .forBrowser('chrome')
+  .build();
   await driver.get('http://www.google.com/ncr');
 } 
 catch(error){
@@ -26,4 +27,5 @@ try{
 testBrowser()
 }catch(error){
   console.log(error)
+  throw error
 }
