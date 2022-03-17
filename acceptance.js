469ce555b8a231eccdc0047cf63017d52a4a8a9a
Kai <kejinko012@gmail.com>
2022-03-17 21:06:57 +0100

    install webdriverio

--- acceptance.js
+++ acceptance.js
@@ -5,19 +5,3 @@ const should = chai.should();
 const expect = chai.expect;
 require("chromedriver")
 var webdriver = require('selenium-webdriver');
-
-async function testBrowser(){
-  try{
-    var driver = new webdriver.Builder().withCapabilities(webdriver.Capabilities.chrome())
-    await driver.get("http://www.google.com")
-  }
-  catch(err){
-    throw err
-  }finally{
-    await driver.quit()
-
-  }
-
-
-
-}
