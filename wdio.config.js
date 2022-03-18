e77648b1cbbba2cce675556354ecfdfcfdfc314e
Kai <kejinko012@gmail.com>
2022-03-18 11:18:46 +0100

    add localhost

--- wdio.config.js
+++ wdio.config.js
@@ -18,6 +18,9 @@ exports.config = Object.assign({}, {
     // need to define host and port information because WebdriverIO can figure that out
     // according to your user and key information. However, if you are using a private Selenium
     // backend you should define the host address, port, and path here.
+    host: 'localhost',
+    port: 4444,
+    path: '/wd/hub',
 
     //
     // ==================
