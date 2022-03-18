8d966a63cbe1a4852cbe1c0eece257cad205b3a1
Kai <kejinko012@gmail.com>
2022-03-18 09:17:56 +0100

    remove host

--- wdio.config.js
+++ wdio.config.js
@@ -26,9 +26,6 @@ exports.config = Object.assign({}, {
     // according to your user and key information. However, if you are using a private Selenium
     // backend you should define the host address, port, and path here.
 
-    port: 4444,
-    path: "wd/hub",
-
     //
     // ==================
     // Specify Test Files
@@ -137,8 +134,7 @@ exports.config = Object.assign({}, {
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
     services: [
-        ['selenium-standalone', { drivers: { firefox: '0.29.1', chrome: true, chromiumedge: 'latest' } }]
-     
+      
     ],//
     firefoxProfile: {
         // Prevent automatic tests with Firefox from showing up in Analytics
