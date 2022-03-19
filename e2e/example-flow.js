3ba9e0f01fc8472c6d836af331d8c3dcf5259837
Kai <kejinko012@gmail.com>
2022-03-19 11:55:51 +0100

    remove default tests

--- e2e/example-flow.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    '@tags': ['example-flow'],
-    'example flow': function (browser) {
-        const login = browser.page.login()
-        //additional steps
-        browser.end();
-    },
-};
