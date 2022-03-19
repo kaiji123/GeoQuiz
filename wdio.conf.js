8b316010ee494c3f5041633fb6f9d8cf3d75f2ea
Kai <kejinko012@gmail.com>
2022-03-19 10:03:17 +0100

    add server

--- wdio.conf.js
+++ wdio.conf.js
@@ -61,11 +61,6 @@ exports.config = {
     //
     capabilities: [{
         browserName: 'chrome',
-        chromeOptions: {
-        // to run chrome headless the following flags are required
-        // (see https://developers.google.com/web/updates/2017/04/headless-chrome)
-            args: ['--headless', '--disable-gpu'],       
-        }        
     }, 
     ],
     //
