234cc9b9b83e22ccc43c1f08b38f8b83eaf5aa4b
Kai <kejinko012@gmail.com>
2022-03-19 02:32:33 +0100

    added docker host

--- seleniumDriver.js
+++ seleniumDriver.js
@@ -1,28 +1,6 @@
 const {Builder, By, Capabilities, Key, until} = require('selenium-webdriver');
 
-
-
-      
-function example() {
-    
-    let driver =  new Builder().forBrowser("chrome")
-    .withCapabilities(Capabilities.chrome())
-    .usingServer('http://68.183.42.162:4444/wd/hub')
+let driver = new webdriver.Builder()
+    .forBrowser('firefox')
+    .usingServer('http://68.183.42.162:4444')
     .build();
-
-       try {
-           
-        driver.get('http://www.linode.com/docs');
-        driver.findElement(By.name('q')).sendKeys('nginx', Key.RETURN);
-        let el = driver.findElement(By.linkText('How to Configure nginx'));
-        
-      } finally {
-         driver.quit();
-      }
-};
-
- function main() {
-    example();
-}
-
-main();
