0667d363caa467e1de91149c967ccb59c2af4a6a
Kai <kejinko012@gmail.com>
2022-04-01 20:40:48 +0200

    added confetti

--- public/scripts/scores.js
+++ public/scripts/scores.js
@@ -1,5 +1,6 @@
 $(function(){
     showScore();
+    startConfetti();
 })
 
 //gets a user's most recent score and displays it
@@ -8,4 +9,22 @@ function showScore(){
     var score = new URLSearchParams(queryString).get("score")
 
     $('#score').html(score + '/10')
+}
+
+function startConfetti(){
+  
+    var myCanvas = document.getElementById("my-canvas")
+    console.log(myCanvas)
+    var myConfetti = confetti.create(myCanvas, {
+        resize: true,
+        useWorker: true
+      });
+      myConfetti({
+        particleCount: 100,
+        spread: 160
+        // any other options from the global
+        // confetti function
+      });
+    console.log("start confetti")
+    myConfetti()
 }
