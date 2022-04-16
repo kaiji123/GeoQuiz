f9fcd4b5f1b86cc628fa2ae9879c2d896e1f7ace
Kai <kejinko012@gmail.com>
2022-04-16 18:44:29 +0200

    login after delete fix

--- public/scripts/client-login.js
+++ public/scripts/client-login.js
@@ -45,19 +45,23 @@ function onSignIn(googleUser) {
         //query the api with the user's id and add them to the db if they're new
         addUserIfNew(id, name).then((res) => {
             // if have a bad response then do not login and stay on the login page
+            console.log("adding user")
             if (res === 403){
+                console.log("add error")
                 return
             }
             else{
+                
                 //get the token and set it in the session storage
                 res.json().then(data=>{
+                    console.log(data)
                     sessionStorage.setItem("token", data)
 
                     //once the user has been added/verified, check their gdpr status
                     //if the user has correct credentials then checkGDPR and use the promise to change the url
                     checkGDPR(profile.getId()).then((gdpr) => {
                         sessionStorage.setItem('gdpr', gdpr)
-                    
+                        console.log("hello")
                         window.location.href = "/"
                     })
                 })   
