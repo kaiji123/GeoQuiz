89f1345f0c64a915c9a119a7eafcec0ea3cee16d
Kai <kejinko012@gmail.com>
2022-04-26 04:59:23 +0200

    scale1

--- e2e/homepage.js
+++ e2e/homepage.js
@@ -2,7 +2,7 @@ module.exports = {
     'example flow': function (browser) {
         browser.url("https://geo-quiz.xyz/")
         browser.waitForElementVisible('.start-quiz')
-        browser.click('.start-quiz')
+        browser.click('#scale1')
         //additional steps
     },
 };
